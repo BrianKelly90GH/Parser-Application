583343f0fe0b45ff13de83601915e6adcc5c6b20
Brian Kelly <briankelly90united@gmail.com>
2018-12-01 18:23:22 +0000

    guard against Determiner in NP i think is done

--- parser.js
+++ parser.js
@@ -14,45 +14,67 @@ const readline = require('readline').createInterface({
 const getLexiconFile = require('./lexicon-file-function');
 
 /**
- * TODO: if there is time use express generator to create a simple web-app using pug
- * Ask the user a question
+ * call the getfile method in the lexicon-file-function file
+ * once the function is executed(.then (used the returned data) ) 
  */
-readline.question(`Please enter a sentence to be parsed:  `, sentence => {
+getLexiconFile.getFile().then(lexiFile => {
 
-    //sentenceArray for storing the users sentence split up into array segments
-    let sentenceArray;
-
-    //lexiFile for string the lexicon parser rules
-    let lexiFile;
-
-    //get the lexicon entries formated the array of objects on the lecture 4 notes
-    getLexiconFile.getFile().then(file => {
-
-        //formated the array of objects on the lecture notes
-        lexiFile = file;
-
-        console.log(lexiFile);
-    });
-
-    //split the sentence in to an array of string of testing it against the lexicon entries
-    sentenceArray = sentence
-        .toLowerCase()
-        .split('.')
-        .join('')
-        .split(' ');
-
-    console.log(sentenceArray);
+    console.log(lexiFile);
 
     /**
-     * Psuedo-code for the grammar. Based on rules_1.png
-     * 
-     * if sentenceArray < 4 it has to be a noun phase
-     * check against the lexicon entries
-     * 
-     * if error print failure
-     * 
-     * else print success
+     * TODO: if there is time use express generator to create a simple web-app using pug
+     * Ask the user a question
      */
+    readline.question(`Please enter a sentence to be parsed:  `, sentence => {
 
-    readline.close();
+        //sentenceArray for storing the users sentence split up into array segments
+        let sentenceArray;
+
+        //split the sentence in to an array of string of testing it against the lexicon entries
+        sentenceArray = sentence
+            .toLowerCase()
+            .replace(/  +/g, ' ')
+            .replace('.', ' ')
+            .trim()
+            .split(' ');
+
+        /**
+         * Psuedo-code for the grammar. Based on rules_1.png
+         * 
+         * if sentenceArray < 4 it has to be a noun phase
+         * check against the lexicon entries
+         * 
+         * if error print failure
+         * 
+         * else print success
+         */
+
+        if (sentenceArray.length < 4) {
+            /**
+             * Psuedo-code for the grammar of a noun phase. Based on rules_1.png
+             * 
+             * if the first segment in the sentenceArray is = to one of the Determiners
+             * set the noun_phase array fist segment to the article
+             * 
+             * else print failure
+             */
+
+            //for storing a noun phase sentence or section of sentence
+            let noun_phase;
+
+            //for storing the start of the sentence Determiners /articles (the or a)
+            let article;
+
+            article = lexiFile.filter(entries => entries.root === sentenceArray[0]);
+
+            if (article.length === 0) {
+                console.log('Sentence is not correct')
+            } else if (article[0].part_of_speech.includes('DET')) {
+                console.log(article);
+            } else {
+                console.log('Sentence is not correct')
+            }
+        }
+        readline.close();
+    });
 });
